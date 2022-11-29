f40a4a1218f5e334427ce0c8738df8ee4b942671
Morning <morning@mtars-lab2-17s-iMac.local>
2022-11-29 12:32:57 +0800

    day11

---
+++ day11/js/main.js
@@ -0,0 +1,212 @@
+// var a = 6;
+
+// console.log(a);
+
+
+
+// var Baby = "Baby shark doo doo doo doo";
+// console.log(Baby);
+
+// console.log(Baby);
+
+// console.log(Baby);
+
+// var a = "Baby shark";
+// var b = "doo doo";
+// var c = "doo doo";
+// var d = "Momy";
+
+// console.log(a + b + c + d);
+
+// //typeof
+// let nullVariable = null;
+// let undify;
+// let myDecision = true;
+
+// console.log(myDecision)
+// console.log( typeof myDecision)
+// console.log(typeof 1);
+// console.log(typeof hello);
+// console.log(typeof undify);
+// console.log(typeof nullVariable);
+
+// //alert gargana
+
+// var x = 4;
+// var y = 3;
+// var z = 5;
+
+// // alert(x * x + y * y - z * z * z / 240 * 175);
+
+//  console.log(7 % 2);
+//  console.log(36 % 19);
+
+//  let result = '30' - 10;
+//  console.log(result);
+
+// const PI = 3.14;
+// let R = 4;
+
+
+// console.log(2 * PI * R *R);
+// alert(2 * PI * R * R);
+
+// let l = 20;
+// let j = 30;
+// let h = 2;
+
+// let bottom = prompt('please give me your bottom!');
+// let test = prompt('please give me your test!');
+// let height = prompt('please give me your height!')
+// let trapezoidArea = (bottom + test ) / 2 * height;
+
+// console.log((l + j) / 2 * h);
+// alert((l + j) / 2 * h);
+// let a  = prompt('Please give me your mean');
+// let b = prompt('Please give me your mean');
+
+// if(a > b);
+
+
+
+
+// if(true){
+//   console.log('its true');
+// } else {
+//   console.log(false);
+// }
+
+// const yearsofbirth = prompt('Year of Birth');
+// const currentYear = 2022;
+
+// const old = currentYear - yearsofbirth;
+// console.log(old);
+
+
+// if(0 <= old && old <= 1){
+//     console.log("Infant");
+// }
+// else if(1 < old && old <= 3){
+//     console.log("Toddler");
+// }
+// else if(5 <= old && old <= 12){
+//     console.log("Preschool");
+// }
+// else if(12 <= old && old <= 18){
+//     console.log("Teen");
+// }
+// else if(18 <= old && old <= 21){
+//     console.log("Youngadult");
+// }
+// else {
+//     console.log("Adult");
+// }
+
+
+// const a = prompt("Please give me number");
+ 
+
+
+// if( a % 2 == 0){
+//     console.log("It's even number");
+// }
+// else{
+//     console.log("It is't even number")
+// }
+
+
+// const b = prompt("Please give me number");
+
+// if(b % 3 == 0 ||  b % 7 == 0 ){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no")
+// }
+
+// const a = prompt("Please give me your weight");
+// const b = prompt("Please give me your height");
+// const BMI = a / (b * b);
+// console.log(BMI);
+
+
+// if( a / (b * b) <= 19){
+//     console.log("Under weight");
+// }
+// else if(a / (b * b) <= 25){
+//     console.log("Normal")
+// }
+// else if(a / (b * b) <= 30){
+//     console.log("Over weight");
+// }
+// else if(a / (b * b) <= 35){
+//     console.log(" Obesity class 1");
+// }
+// else if(a / (b * b) <= 40){
+//     console.log(" Obesity class 2");
+// }
+// else if(a / (b * b) <= 45){
+//     console.log(" Obesity class 3");
+// }
+
+
+// let day = prompt("Give random day");
+// let dayNumber = Number(day);
+// console.log(typeof(dayNumber));
+
+// console.log(typeof(day))
+// let dayName;
+
+// if (day == 1 ){
+//      dayName = 'Monday';
+// }
+
+// switch(dayNumber){
+//     case 1:
+//         dayName = 'Monday';
+//         break;
+//     case 2:
+//         dayName = 'Tuesday';
+//         break;
+//     case 3:
+//         dayName = 'Wednesday';
+//         break;
+//     case 4:
+//         dayName = 'Tusday';
+//         break;
+//     case 5:
+//         dayName = 'Friday';
+//         break;
+//     case 4:
+//         dayName = 'Saturday';
+//         break;
+//     case 4:
+//         dayName = 'Sunday';
+//         break;
+    
+
+
+//     default: 
+//     dayName = 'ivnvalid day';
+// }
+// console.log(dayName);
+
+
+
+const n = prompt("Please give me some number");
+console.log(n)
+
+if(n < 0){
+    console.log("negative");
+}
+else if (n > 0){
+    console.log("positive");
+}
+
+let input = prompt('data');
+if (Number(input) == NaN){
+    alert('number');
+}
+else{
+    console.log(Number(input));
+}
