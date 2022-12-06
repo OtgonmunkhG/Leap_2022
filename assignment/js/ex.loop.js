66aa2ea36a8ce5cd98d0dd53e3debac34bfe0116
OtgonmunkhG <g.otgonmunkh@gmail.com>
2022-12-06 20:03:36 +0800

    loop

---
+++ assignment/js/ex.loop.js
@@ -0,0 +1,7 @@
+// let randomNumber = Math.random();
+console.log(Math.floor(Math.random()* 100 ));
+for(let i = 0; i <= Math.floor(Math.random()* 100 ); i++ ){
+    if(i == Math.floor(Math.random()* 100 ) ){
+        console.log(i);
+    }
+}
