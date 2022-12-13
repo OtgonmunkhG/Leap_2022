a4500f716cc7104c442e3c164bf86f720de493b5
Morning <morning@mtars-lab2-17s-iMac.local>
2022-12-13 12:22:35 +0800

    assignment

--- J-DOM/Day20/js/events.js
+++ J-DOM/Day20/js/events.js
@@ -1,54 +1,49 @@
-console.log("events");
-let myButton = document.getElementById("myButton");
-console.log(myButton);
 
 
-
-let color = "white";
-// myButton.style = `background-color: ${color}`;
-function changeBackgroundColors (){
-    console.log("clicked");
-    if(color === "white "){
-        myButton.style = `background-color: green `;
-        color = "green";
-    } else{
-        
-        myButton.style =`background-color: white`;
-        color = "white";
+function validation(inputs) {
+    // энд та нарт бүх input орж ирж байгаа зөв эсэхийг дараах байдалаар шалгана
+    let password = inputs.psw;
+    console.log(psw.value);
+    let repeatPassword = inputs.repeatPsw;
+    console.log(repeatPassword.value);
+    let email = inputs.email;
+    console.log(email);
+    if(password.value === repeatPassword.value && password.length >= 8 && repeatPassword.length >= 8 && email.value.includes("@") && email.value.includes(".")){
+        return true;
+       
     }
+   
+    // 2 password хоорондоо адил байгаа эсэх
+    
+    // password ийн урт 8с урт байх ёстой
 
-}
-myButton.style =`background-color: ${color}`;
-// function changeColors(){
-//     console.log("clicked");
-//     if(color === "green"){
-//         myButton.style = `background-color: ${color}`;
-//         color = "white";
-//     } else{
-//         myButton.style =` background-color: ${color}`;
-//         color = "green";
-//     }
-// }
-// myButton.addEventListener("click", changeColors);
-//bagshiin codiig harna uu!
+    // email дотор заавал @ болон . орсон байх
+    console.log(inputs.email.value);
 
-
-
-
-    // // changeBackgroundColor();
-    // // myButton.addEventListener("click", changeBackgroundColorGreen);
-
-    // let click = myButton.addEventListener("click", changeBackgroundColorGreen);
-    // // let doubleClick = myButton.addEventListener("click", changeBackgroundColorWhite);
+    // эдгээр нөхцөл биелсэн бол true буцаана
   
-// let arrowFunc = () => {
-
-// }
-// myButton.addEventListener("click", () => {
-//     myButton.style ="background-color: red;"
-// } )
-
-
-// let registerButton = document.querySelector("#register");
-// console.log(registerButton);
-// registerButton.disabled = true;
+    //жишээ хэрэглээ
+    console.log("Email input ийн урт: ", inputs.email.value.length);
+    return false;
+  }
+  
+  // бүх input үүдийг inputs-д авч байна
+  const inputs = document.getElementsByTagName("input");
+  // хэдэн input байгааг тоолж байна
+  const howManyInputs = document.getElementsByTagName("input").length;
+  
+  for (let i = 0; i < howManyInputs; i++) {
+    // input бүрт addEventListener өгч change эвэнтийг сонсож байна
+    inputs[i].addEventListener("change", () => {
+      // input ийн утга өөрчлөгдөх бүрт 3н input үүдийн утгуудыг харж болно
+      console.log("Email-ийн утга: ", inputs.email.value);
+      console.log("Password-ийн утга: ", inputs.psw.value);
+      console.log("repeatPsw-ийн утга: ", inputs.repeatPsw.value);
+  
+      // бүх input үүдийг авч зөв эсэхийг шалгах фүнкц дуудаж байна
+      if (validation(inputs)) {
+        document.querySelector(".registerBtn").disabled = false;
+      }
+    });
+  }
+//   console.log("test");
---
+++ J-DOM/day21/js/bom.js
@@ -0,0 +1,50 @@
+// console.log("BOM.js");
+// // // window.alert("Hello");
+// // console.log(window);
+// // window.open("https://google.com", "Hello");
+// // window.confirm("hello");
+// // let result = window.confirm("Do you wanna to King?");
+// // console.log(result);
+// // let message = result ? "you click ok ":
+// // "You click cancel";
+
+// // alert(message);
+
+// // //promt
+// // window.prompt("give me a number");
+
+// //set time out
+let alertButton = document.getElementById("alertButton");
+let cancelButton = document.getElementById("cancelButton");
+let timeoutID;
+
+alertButton.addEventListener("click", () => {
+    console.log("alert button");
+    timeoutID = setTimeout(alert, 3000, 'setTimeout Demo');
+});
+cancelButton.addEventListener("click", () => {
+    console.log(timeoutID);
+    window.clearTimeout(timeoutID);
+})
+console.log(timeoutID);
+
+setTimeout(() => {
+    console.log("Time out is running");
+}, 5000)
+let counter = 0;
+// setInterval(startInterval, 1000);
+let startMyTimer = document.getElementById("startTimer");
+startMyTimer.addEventListener("click", runTimerInterval);
+
+function runTimerInterval (){
+    window.setInterval(startInterval, 1000);
+
+}
+
+ function startInterval(){
+    let now = new Date();
+    let mytimer = document.getElementById("mytimer");
+    mytimer.innerHTML = now;
+    console.log(counter);
+    counter++;
+ }
