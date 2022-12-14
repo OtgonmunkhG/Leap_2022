4465362347a4ca1edd08c7e0f01e815baf126caa
OtgonmunkhG <g.otgonmunkh@gmail.com>
2022-12-14 22:25:50 +0800

    nemelt

---
+++ J-DOM/Day20/js/assignment/js/ex04.js
@@ -0,0 +1,8 @@
+console.log("random color generator");
+let colors = document.getElementsByTagName("body");
+let bgColor = "white";
+if(colors === bgColor){
+    document.getElementsByTagName("body").style = "background-color: red".innerHTMl;
+} else if(colors !== bgColor ){
+    document.getElementsByTagName("body").style ="background-color:blue".innerHTMl;
+}
