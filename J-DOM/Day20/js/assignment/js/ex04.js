270780bf2a4c8683a179171d7cad94160d2f063a
OtgonmunkhG <g.otgonmunkh@gmail.com>
2022-12-16 22:13:30 +0800

    gert

--- J-DOM/Day20/js/assignment/js/ex04.js
+++ J-DOM/Day20/js/assignment/js/ex04.js
@@ -2,7 +2,7 @@ console.log("random color generator");
 let colors = document.getElementsByTagName("body");
 let bgColor = "white";
 if(colors === bgColor){
-    document.getElementsByTagName("body").style = "background-color: red".innerHTMl;
+    document.getElementsByTagName("body").style = "background-color: red"
 } else if(colors !== bgColor ){
-    document.getElementsByTagName("body").style ="background-color:blue".innerHTMl;
+    document.getElementsByTagName("body").style ="background-color:blue"
 }
