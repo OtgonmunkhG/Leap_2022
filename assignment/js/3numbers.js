5b28b22d806512cbc698d98bf2b44e1c511351de
OtgonmunkhG <g.otgonmunkh@gmail.com>
2022-12-17 21:52:31 +0800

    gert

---
+++ assignment/js/3numbers.js
@@ -0,0 +1,16 @@
+console.log("гурван тоон ");
+
+let a = 89;
+let b = 45;
+let c = 10;
+if(a > b && a > c ){
+    console.log(a);
+} else if(b > a && b > c){
+    console.log(b);
+} else {
+    console.log(c);
+}
+let max = 0;
+while(max <= a ){
+    console.log(a);
+}
---
+++ assignment/js/average-score.js
@@ -0,0 +1,17 @@
+console.log("average score");
+let aTeam = 96 + 108 + 189;
+console.log(aTeam / 3);
+let bTeam = 88 + 191 + 110;
+console.log(bTeam / 3);
+
+
+
+if(aTeam / 3 >= 100 && bTeam / 3 >= 100 ){
+console.log("enought");
+let result = aTeam / 3 <= bTeam / 3   ?  console.log("B баг түрүүлсэн") : console.log("A баг түрүүлсэн");
+ 
+} else {
+    console.log("Not winner");
+}
+
+
