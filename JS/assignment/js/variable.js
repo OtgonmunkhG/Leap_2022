57711a85338063045e85279be26226ea54baabc3
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-01-14 21:56:28 +0800

    variable

---
+++ JS/assignment/js/variable.js
@@ -0,0 +1,30 @@
+let a = 97;
+let b = 0;
+let c = 23;
+let d = 6;
+let sum = a + b + c + d;
+a <= 80 && b <=80 && c <= 80 && d <= 80 ? console.log(sum) : console.log("that's all of vast of 80");
+if(a < 80 && b < 80 && c < 80 && d < 80){
+    console.log(a + b + c + d);
+} else {
+    console.log("something wrong");
+}
+switch(a && b && c && d <= 80) {
+    case a: console.log(a + b + c + d);
+    break;
+
+    case b: console.log("something wrong");
+
+}
+let num = 4;
+let number = 4;
+let too = 2;
+let num2 = 4;
+let count = 0;
+
+if( num <= 5 && number <= 5 && too <=5 && num2 <= 5  ) {
+    console.log(num * number * too * num2 );
+    
+} else {
+    console.log("hello");
+}
