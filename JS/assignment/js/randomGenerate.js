5fe0c020c84b21a8cf568076a143f2372b25325c
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-01-16 18:17:01 +0800

    assignment

---
+++ JS/assignment/js/randomGenerate.js
@@ -0,0 +1,5 @@
+let i = Number(prompt("Please give me number"));
+let suggest = Number(prompt("Please give me number"));
+i = Math.floor(Math.random() * 1000 - 2);
+suggest = Math.floor(Math.random() * 890 - 100);
+console.log("can i suggest the numbers, because this numbers return a lucky for you" , suggest , i);
