91a0e438c303f9494a5b5ff8b0096cf46b7ad264
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-01-16 16:05:53 +0800

    random number

---
+++ JS/assignment/js/randomNumber.js
@@ -0,0 +1,7 @@
+let randomN = Math.floor(Math.random()* 100)
+console.log(randomN);
+let randomNumber = Math.floor(Math.random() * 255);
+console.log(randomNumber);
+let min = 50;
+let max = 255;
+console.log(Math.floor(Math.random() * (max - min ) + min));
