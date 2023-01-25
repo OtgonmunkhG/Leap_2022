156ae318e2012a974a91467953bb3e30c83c9c90
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-01-25 19:24:19 +0800

    added simple function

---
+++ JS/assignment/function/simpleFunction.js
@@ -0,0 +1,22 @@
+
+
+function findName() {
+    const lastName = "Otgonmunkh"
+    console.log(lastName)
+}
+findName()
+function fullName(lastName, firstName) {
+    return lastName + firstName
+}
+console.log(fullName("otgoo", "saraa"))
+
+function findSum(x, y) {
+    return x + y
+}
+console.log(findSum(189, 89))
+
+function findRectangleArea(l, w) {
+    return (l * w)
+}
+console.log(findRectangleArea(890, 12))
+
