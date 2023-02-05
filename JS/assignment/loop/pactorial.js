4fd38bc888b3d3836ab9a4e5298dca552d7b541b
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-02-05 21:25:17 +0800

    added with loop assignment

---
+++ JS/assignment/loop/pactorial.js
@@ -0,0 +1,10 @@
+console.log("hello")
+
+let pactorial = Number(prompt("Please add number"))
+
+let multify = 1;
+for(let i = 1; i <= pactorial ; i++) {
+        multify = multify * i
+
+        console.log(multify)
+}
